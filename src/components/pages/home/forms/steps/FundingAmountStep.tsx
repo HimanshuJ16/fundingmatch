1dd6a3a1da227e7bd9ae329ffc0903e7fc5584d8
unknown <himanshujangir16@gmail.com>
2026-01-14 00:35:17 +0530

    first commit

--- src/components/pages/home/forms/steps/FundingAmountStep.tsx
+++ src/components/pages/home/forms/steps/FundingAmountStep.tsx
@@ -17,7 +17,7 @@ export const FundingAmountStep = () => {
 
   return (
     <div className="flex flex-col gap-4 w-full">
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-3">
+      <div className="grid grid-cols-2 gap-3">
         {AMOUNTS.map((amount) => (
           <button
             key={amount}
